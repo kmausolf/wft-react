6e100e7df600895e7bb93dfed8967166c1bf4263
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2022-06-09 20:48:18 -0700

    style(contacts): disambiguate variable name

--- day7/context/src/contacts/Contacts.js
+++ day7/context/src/contacts/Contacts.js
@@ -4,12 +4,12 @@ import { useContacts } from "../providers/ContactsProvider";
 const Contacts = (props) => {
     const [name, setName] = useState("");
     const [id, setId] = useState();
-    const contacts = useContacts();
+    const contactsContext = useContacts();
 
     const renderContacts = () => {
         let contactList = [];
 
-        contactList = contacts.contacts.map(contact => {
+        contactList = contactsContext.contacts.map(contact => {
             return <li>{contact.name}, id: {contact.id}</li>
         });
 
@@ -31,9 +31,9 @@ const Contacts = (props) => {
                 {renderContacts()}
             </ul>
             <input type="text" onChange={handleChangeName} placeholder="Enter a name..."/>
-            <button onClick={() => contacts.addContact(name)}>Add Contact</button>
+            <button onClick={() => contactsContext.addContact(name)}>Add Contact</button>
             <input type="text" onChange={handleChangeId} placeholder="Enter an id..."/>
-            <button onClick={() => contacts.deleteContact(id)}>Delete Contact</button>
+            <button onClick={() => contactsContext.deleteContact(id)}>Delete Contact</button>
         </div>
     )
 }
